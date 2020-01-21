4ee302aa73b62c6ed4edc124c0e0bfd2799d327c
Jonathan Bußhoff <jonathan@busshoff.net>
2020-01-21 09:26:19 +0100

    Revert "Add files via upload"
    This reverts commit 725b2a7c983e26126ce0cf7dc0164e3024b6bf4c.

--- src/Aufgabe17/Aufgabe17.java
+++ src/Aufgabe17/Aufgabe17.java
@@ -5,8 +5,8 @@ import java.net.SocketException;
 public class Aufgabe17 {
     public static void main(String[] args) {
         try {
-            Client17 client = new Client17("255.255.255.255", 4998);
-            Server17 server = new Server17(4998);
+            Client17 client = new Client17("255.255.255.255", 7777);
+            Server17 server = server = new Server17(7777);
             server.start();
             client.start();
         } catch (SocketException e) {
